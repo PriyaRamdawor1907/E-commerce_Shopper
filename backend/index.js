543804aa56ad6bf0783c7a77e56034faf45f3771
Priya-Ramdawor <priya.ramdawor@spoonconsulting.com>
2025-03-30 22:07:11 +0400

    added API endpoint for User signUp/login

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,7 @@ const multer = require("multer");
 const path = require("path");
 const cors = require("cors");
 const { type } = require("os");
+const { error } = require("console");
 
 app.use(express.json());
 app.use(cors());
@@ -122,6 +123,81 @@ app.get('/allproducts', async (req, res)=>{
 	res.send(products);
 })
 
+//user schema
+const Users = mongoose.model('Users', {
+	name:{
+		type:String,
+	},
+	email:{
+		type:String,
+		unique:true
+	},
+	password:{
+		type:String,
+	},
+	cartData:{
+		type:Object
+	},
+	date:{
+		type:Date,
+		default:Date.now,
+	}
+})
+
+//API to create user
+app.post('/signup', async (req,res)=>{
+	let check = await Users.findOne({email:req.body.email});
+	if(check) {
+		return res.status(400).json({
+			success:false, errors: "Existing User found with same Email Address"
+		})
+	}
+	let cart = {};
+	for (let i=0; i<300; i++){
+		cart[i]=0;
+	}
+	const user = new Users({
+		name:req.body.username,
+		email:req.body.email,
+		password:req.body.password,
+		cartData:cart,
+	})
+
+	await user.save();
+
+	const data = {
+		user:{
+			id:user.id
+		}
+	}
+
+	const token = jwt.sign(data, 'secret_ecom')
+	res.json({success:true, token})
+})
+
+//API for user login
+app.post('/login', async (req,res)=>{
+	let user = await Users.findOne({email:req.body.email});
+	if (user){
+		const passCompare = req.body.password === user.password;
+		if (passCompare){
+			const data = {
+				user:{
+					id:user.id
+				}
+			}
+
+			const token = jwt.sign(data, 'secret_ecom')
+			res.json({success:true, token})
+		}
+		else{
+			res.json({success:false, errors:"Wrong password"})
+		}
+	}
+	else{
+		res.json({success:false, errors:"Wrong Email Id"})
+	}
+})
 
 //start express server
 app.listen(port, (error)=>{
